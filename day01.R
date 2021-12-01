0a7a1355d7370f1b1824b0fc506e2a46e5601f06
exunckly <3117100+exunckly@users.noreply.github.com>
2021-12-01 21:50:27 +0000

    Fixed issue with pushing from RStudio. We now have the shortest part 2 yet.

--- day01.R
+++ day01.R
@@ -10,7 +10,7 @@ input_filename <- "day01_test.txt"
 #input_filename <- "day01_input.txt"
 
 my_data <- as.numeric(readLines(here("data", input_filename)))
-part1 <- sum(sign(diff(my_data)) == 1)
+part1 <- sum(diff(my_data) > 0)
 
 
 # Part 2
@@ -27,7 +27,6 @@ part1 <- sum(sign(diff(my_data)) == 1)
 # 260      G H
 # 263        H
 #Count the number of times the sum of measurements in this sliding window increases 
-
 
 # We actually want to know how many times the n+3th value is larger than the nth value
 # as the other two values are shared between the sums - don't need to calculate sums or write a loop
@@ -47,20 +46,23 @@ my_data_offset <- my_data[start_index:curr_len]
 my_data_trunc <- my_data[1:new_len]
 
 # Get the answer
-part2 <- sum(sign(my_data_offset - my_data_trunc) == 1)
+part2 <- sum((my_data_offset - my_data_trunc) > 0)
 
 
 # Notes after looking at others' solutions
+
+#Revised part 2 solution - take 1
 
 # The lead and lag functions from dplyr are designed to offset vectors
 # And they pad with NA so we don't have vectors of different lengths - then can use na.omit
 # A more efficient part 2 solution would be:
 
-#Revised part 2 solution
-
 window_len <- 3
 my_data_offset2 <- dplyr::lead(my_data, window_len)
-part2a <- sum(sign(na.omit(my_data_offset2 - my_data)) == 1)
+part2a <- sum(na.omit(my_data_offset2 - my_data) > 0)
 
+#Revised part 2 solution - take 2
+# The diff function allows you to specify an offset, meaning that the syntax can be more or less identical to the part 1 solution
 
+part2b <- sum(diff(my_data, lag = window_len) > 0)
 
